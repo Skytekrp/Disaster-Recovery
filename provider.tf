ddcbd535b78061e4e6be59c1e0acb32489279b53
Abdelmoumene Mameri <_amameri.az@sodexo.com>
2025-10-22 19:12:29 +0200

    update tenantid

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ terraform {
     container_name       = "tfstate"
     key                  = "drp.terraform.tfstate"
     subscription_id      = "6c475893-af6c-42c4-b8a8-0afb5cb79259"
-    tenant_id            = "you211e6e3f-316e-4b8b-9b2c-a856bfa086ca"
+    tenant_id            = "211e6e3f-316e-4b8b-9b2c-a856bfa086ca"
     use_azuread_auth     = true
     use_oidc             = true   # if you use OIDC with GitHub Actions
   }
