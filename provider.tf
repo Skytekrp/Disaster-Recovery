563425926eb38b24c31c87a71ad35702dc4d00f8
Abdelmoumene Mameri <_amameri.az@sodexo.com>
2025-10-22 16:52:45 +0200

    update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,5 +4,9 @@ terraform {
     storage_account_name = "st1state2578"
     container_name       = "tfstate"
     key                  = "drp.terraform.tfstate"
+    subscription_id      = "6c475893-af6c-42c4-b8a8-0afb5cb79259"
+    tenant_id            = "you211e6e3f-316e-4b8b-9b2c-a856bfa086ca"
+    use_azuread_auth     = true
+    use_oidc             = true   # if you use OIDC with GitHub Actions
   }
 }
